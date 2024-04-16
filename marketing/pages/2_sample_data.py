8ddf6f77174acdbda61c0acd8ec1bfeddf136330
h0ssein2011 <h0ssein2011@yahoo.com>
2024-04-16 22:20:20 +0300

    dbug path

--- marketing/pages/2_sample_data.py
+++ marketing/pages/2_sample_data.py
@@ -2,6 +2,6 @@ import streamlit as st
 from pathlib import Path
 import pandas as pd
 path = Path()
-path_csv = path /'data/bank-full.csv'
+path_csv = path.parent /'data/bank-full.csv'
 bank = pd.read_csv(path_csv,sep=';')
 st.write(bank.head())
