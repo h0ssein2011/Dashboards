a3642db51968b087f0b2497152fedd4ef5b4e6da
h0ssein2011 <h0ssein2011@yahoo.com>
2024-04-16 22:30:42 +0300

    add page info

--- marketing/pages/2_sample_data.py
+++ marketing/pages/2_sample_data.py
@@ -3,5 +3,11 @@ from pathlib import Path
 import pandas as pd
 path = Path(__file__).parent.parent
 path_csv = path /'data/bank-full.csv'
+path_additional = path /'data/bank-additional-full.csv'
 bank = pd.read_csv(path_csv,sep=';')
+bank_addition = pd.read_csv(path_additional,sep=';')
+
+st.markdown('### Bank sample data', unsafe_allow_html=True)
 st.write(bank.head())
+st.markdown('### Bank sample additional data', unsafe_allow_html=True)
+st.write(bank_addition.head())
